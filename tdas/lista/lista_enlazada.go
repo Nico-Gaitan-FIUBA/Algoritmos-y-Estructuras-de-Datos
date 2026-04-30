38dd9af9d27b4c7b5326b04186f4418d478af1ea
Nico-Gaitan-FIUBA <ngaitan@fi.uba.ar>
2026-04-29 22:33:12 -0300

    Se agregaron las funciones CrearListaEnlazada,  EstaVacia, InsertarPrimero y InsertarUltimo

--- tdas/lista/lista_enlazada.go
+++ tdas/lista/lista_enlazada.go
@@ -10,3 +10,33 @@ type listaEnlazada[T any] struct {
 	ultimo  *nodo[T]
 	largo   int
 }
+
+func CrearListaEnlazada[T any]() Lista[T] {
+	return &listaEnlazada[T]{}
+}
+
+func (l *listaEnlazada[T]) EstaVacia() bool {
+	return l.primero == nil
+}
+
+func (l *listaEnlazada[T]) InsertarPrimero(valor T) {
+	nuevoNodo := &nodo[T]{dato: valor}
+	if l.EstaVacia() {
+		l.primero = nuevoNodo
+		l.ultimo = nuevoNodo
+	} else {
+		aux := l.primero
+		l.primero = nuevoNodo
+		l.primero.sig = aux
+	}
+}
+
+func (l *listaEnlazada[T]) InsertarUltimo(valor T) {
+	nuevoNodo := &nodo[T]{dato: valor}
+	if l.EstaVacia() {
+		l.primero = nuevoNodo
+	} else {
+		l.ultimo.sig = nuevoNodo
+	}
+	l.ultimo = nuevoNodo
+}
