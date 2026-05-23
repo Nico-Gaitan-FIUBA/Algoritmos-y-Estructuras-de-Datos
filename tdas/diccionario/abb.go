aa44847cf60628c42261ce7b41197ab1620c07b6
Nico-Gaitan-FIUBA <ngaitan@fi.uba.ar>
2026-05-23 19:07:45 -0300

    Se corrige la funcion iterarRango para que se recorra el árbol de la forma esperada

--- tdas/diccionario/abb.go
+++ tdas/diccionario/abb.go
@@ -165,14 +165,20 @@ func (nodo *nodoAbb[K, V]) iterarRango(arbol *abb[K, V], desde *K, hasta *K, vis
 	}
 
 	if arbol.funcion_cmp(nodo.clave, *hasta) > 0 {
-		nodo.izq.iterar(visitar)
+		nodo.izq.iterarRango(arbol, desde, hasta, visitar)
 	}
 
 	if arbol.funcion_cmp(nodo.clave, *desde) > 0 && arbol.funcion_cmp(nodo.clave, *hasta) < 0 {
-		visitar(nodo.clave, nodo.dato)
+		nodo.izq.iterarRango(arbol, desde, hasta, visitar)
+
+		if !visitar(nodo.clave, nodo.dato) {
+			return
+		}
+
+		nodo.der.iterarRango(arbol, desde, hasta, visitar)
 	}
 
 	if arbol.funcion_cmp(nodo.clave, *desde) < 0 {
-		nodo.der.iterar(visitar)
+		nodo.der.iterarRango(arbol, desde, hasta, visitar)
 	}
 }
