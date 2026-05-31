4f66a8702188fd05492e58eebd87ced7d0413ef1
Nico-Gaitan-FIUBA <ngaitan@fi.uba.ar>
2026-05-31 20:01:35 -0300

    Implementa la estructura de datos heap y la función CrearColaPrioridad para la cola de prioridad.

--- tdas/heap/heap.go
+++ tdas/heap/heap.go
@@ -1,1 +1,14 @@
 package cola_prioridad
+
+type heap[T any] struct {
+	arr         []T
+	funcion_cmp func(T, T) int
+}
+
+func CrearColaPrioridad[T any](funcion_cmp func(T, T) int) ColaPrioridad[T] {
+	return &heap[T]{arr: []T{}, funcion_cmp: funcion_cmp}
+}
+
+func (h *heap[T]) EstaVacia() bool {
+	return len(h.arr) == 0
+}
